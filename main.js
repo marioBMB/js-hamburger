73ba03aec07bdf6586197d7dfde868d680feffa0
marioBMB <m.sannicola@studenti.uniba.it>
2021-12-09 16:43:59 +0100

    riuso classe active nel css

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 
-
 let hamburgerBtn = document.querySelector(".header-right > a");
 let hamburgerMenu = document.querySelector(".hamburger-menu");
 let closeBtn = document.querySelector("a.close");
-console.log(closeBtn);
 
 
 hamburgerBtn.addEventListener("click", function(){
-    hamburgerMenu.style.display = "block";
+
+    if (hamburgerMenu.className == "hamburger-menu" ) {
+
+        hamburgerMenu.classList.add("active");
+    }
+    else {
+        hamburgerMenu.className == "hamburger-menu";
+    }
+
+    /* si poteva anche risolvere tutto senza if/else, con:
+     hamburgerMenu.classList.toggle("active")  
+    
+     ma risulta meno chiaro e meno granulare (se voglio ad es. eseguire azioni extra) */
 });
 
+
 closeBtn.addEventListener("click", function(){
-    hamburgerMenu.style.display = "none";
+    
+    hamburgerMenu.className = "hamburger-menu";
 });
 
